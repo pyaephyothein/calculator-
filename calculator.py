4da7a61cbcbb80282766fd1952511f25adcf2489
pyaephyothein <88430200+pyaephyothein@users.noreply.github.com>
2022-02-06 22:33:11 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#num1 = int(input("Enter a number : "))
+#num2 = int(input("Enter a another number : "))
+#result = num1 + num2
+
+#print("The result is " + str(result))
+
+exp = input("Enter your calculation : ")
+print(eval(exp))
